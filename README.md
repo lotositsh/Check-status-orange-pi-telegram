313240d6fdd369e20204883cdfb5227e0ec2dbd7
lotositsh <lotositsh@gmail.com>
2023-03-12 23:44:45 +0300

    old

--- README.md
+++ README.md
@@ -10,7 +10,9 @@ sudo apt-get install htop aha wkhtmltopdf
 ```
 echo q | htop | aha --black --line-fix > htop.html && wkhtmltoimage --width 538 htop.html htop.png 
 ```
-
+The image width can be changed by changing the --width 800 --height 400 parameter to the desired
+***
+Have Error in work service blackscreen =((
 ***
 Telegram bot checks the status of your orange pi, showing the temperature, degree of loading and ip. Added userid validation function.
 Do not forget to add your TOKEN and user ID to the code
--- status.py
+++ status.py
@@ -4,13 +4,11 @@ from aiogram import Bot, Dispatcher, types, executor
 from aiogram.dispatcher.filters import Command
 import subprocess
 import time
-from config import *
-
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
 # Create a Telegram bot instance
-bot = Bot(token=TOKKEN)
+bot = Bot(token='YOUR_TOKEN_HERE')
 
 # Set up the dispatcher
 dp = Dispatcher(bot)
@@ -19,8 +17,9 @@ dp = Dispatcher(bot)
 @dp.message_handler(Command('status'))
 async def status(message: types.Message):
     user_id = message.from_user.id
+    allowed_user_id = 777777777  # Replace with your user ID
 
-    if user_id != USER_ID:
+    if user_id != allowed_user_id:
         await message.answer("You are not authorized to run this command.")
         return
     # Run the command to get system status and store the output
@@ -31,10 +30,10 @@ async def status(message: types.Message):
         # "echo 'CPU usage: ' && mpstat -P ALL | awk '{if($2 ~ /[0-9]+/ && $2 < 4) print \"CPU\",$2\":\",100-$NF\"%\"}' && "
         "echo 'IP address: ' && curl -s ifconfig.co && echo ' ('$(curl -s ifconfig.co/country)')'",
         "r").read()
-    time.sleep(1)
+    time.sleep(2)
     command = "echo q | htop | aha --black --line-fix > htop.html && wkhtmltoimage --width 538 htop.html htop.png"
     subprocess.run(command, shell=True, check=True)
-    time.sleep(3)
+    time.sleep(2)
 
 
     # Send the output back to the user
@@ -42,11 +41,11 @@ async def status(message: types.Message):
         await message.answer(cmd_output)
         with open('htop.png', 'rb') as photo:
             # Send the photo using the bot
-            await bot.send_photo(USER_ID, photo)
+            await bot.send_photo(allowed_user_id, photo)
 
     except:
     # Handle other errors
-        await bot.send_message(USER_ID, "An error occurred")
+        await bot.send_message(allowed_user_id, "An error occurred")
 
 # Start the bot
 if __name__ == '__main__':
